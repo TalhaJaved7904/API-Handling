fd50850de61998b3ad92d9234b3199222c27e588
TalhaJaved7904 <149853918+TalhaJaved7904@users.noreply.github.com>
2024-06-01 13:02:53 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Apihandlingscreen from './pages/apihandlihngscreen';
+
+function App() {
+  return (
+    <div>
+      
+      <Apihandlingscreen />
+    
+    </div>
+  );
+}
+
+export default App;
